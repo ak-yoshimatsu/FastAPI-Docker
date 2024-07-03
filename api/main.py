fb8665775b4b47ced6d9d5825cfdc534f2c79c87
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2024-07-04 08:30:48 +0900

    api追加

--- api/main.py
+++ api/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+
+class UserRequest(BaseModel):
+    name: str = Field(max_length=10, examples=["山田太郎"])
+    age: int = Field(examples=[29])
 
 
 @app.get("/")
 def index():
     return {"data": "Hello!!!"}
+
+
+@app.post("/", response_model=UserRequest)
+def store(request: UserRequest):
+    return request
+
+
+@app.get("/test")
+def test():
+    return {"data": "テストです！！"}
